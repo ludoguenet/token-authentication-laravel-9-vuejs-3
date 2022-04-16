3c6d38bbc125fd5128703602bce04c82002f46fc
ludoguenet <ludovicguenet@gmx.com>
2022-04-16 20:38:03 +0200

    refactor to API Composition

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -1,28 +1,33 @@
 <template>
     <router-link :to="'/login'">Login</router-link>
-    <h1 v-if="isAdmin()">Bienvenue Administrateur!</h1>
+    <h1 v-if="isAdmin">Bienvenue Administrateur!</h1>
     <h1 v-else>Dashboard</h1>
     <button @click="getInformations">Obtenir mes informations</button>
 </template>
 
 <script>
+import { computed, onMounted } from '@vue/runtime-core';
 import axiosClient from '../axios';
 import store from '../store';
 
 export default {
-    name: "Dashboard",
-    methods: {
-        getInformations() {
-            axiosClient.post('/privateInformations')
+    setup() {
+        const getRole = async () => {
+            await store.dispatch('getRole');
+        }
+
+        const getInformations = async () => {
+            await axiosClient.post('/privateInformations')
                 .then(res => console.log(res))
                 .catch(err => console.log(err));
-        },
-        isAdmin() {
-            return store.state.user.data.role;
         }
-    },
-    mounted() {
-        store.dispatch('getRole');
+
+        onMounted(getRole());
+        
+        return {
+            isAdmin: computed(() => store.state.user.data.role),
+            getInformations
+        }
     }
 }
 </script>
